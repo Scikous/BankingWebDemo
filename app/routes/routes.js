af9ec9ce58691abaaa99937954c7c8b34644acf6
Scikous <95752605+Scikous@users.noreply.github.com>
2023-07-10 21:44:18 +0300

    Current state of project

---
+++ app/routes/routes.js
@@ -0,0 +1,15 @@
+import { Router } from "https://deno.land/x/oak@v11.1.0/mod.ts";
+import * as authenticationController from "./controllers/authenticationController.js";
+import * as mainController from "./controllers/mainController.js";
+import * as accountsController from "./controllers/accountsController.js";
+const router = new Router();
+
+router.get("/", mainController.showMain);
+
+router.get("/auth/register", authenticationController.showRegistrationForm);
+router.post("/auth/register", authenticationController.postRegistrationForm);
+router.get("/auth/login", authenticationController.showLoginForm);
+router.post("/auth/login", authenticationController.postLoginForm);
+router.get("/accounts", accountsController.showAccounts);
+router.post("/accounts", accountsController.accountCreateForm);
+export { router };
